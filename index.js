e76036f4199b1f131017dd15ba41f401444e81dd
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-01 01:27:21 +0000

    Deploying to master from @ dimaslanjaka/hexo-blogger-xml@26c458b2f68dbea410861175fc679c0a0b4d91cd 🚀

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const fs = require("fs");
 //hexo.extend.filter.register("after_render:html", require("./lib/parser"), 5);
 //hexo.extend.filter.register("after_render:html", require("./dist/hexo.test.js"), 5);
-if (typeof hexo != "undefined") require("./dist/hexo-core.js")(hexo);
+if (typeof hexo != "undefined") {
+  if (fs.existsSync("./build/dist/hexo-core.js")) {
+    // development mode
+    require("./build/dist/hexo-core.js")(hexo);
+  } else {
+    // publish mode
+    require("./dist/hexo-core.js")(hexo);
+  }
+}
+
 /**
  * gulp function
  * @type {(function(gulpConfig): void)}
  */
 const gulpFunction = require("./dist/gulp-core");
+const { existsSync } = require("fs");
 module.exports = {
   gulp: gulpFunction,
 };
