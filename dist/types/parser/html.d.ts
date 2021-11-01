54cb7c22c075e4d377cd489bd83b2b95d0d98b49
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-01 07:07:32 +0700

    Migrate to compiler branch

--- dist/types/parser/html.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { PathLike } from "fs";
-import { JSDOM } from "jsdom";
-export declare function fromFile(path: string | PathLike): false | JSDOM;
-export declare function fromString(str: string): JSDOM;
-export declare function title(str: string): string;
