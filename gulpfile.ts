d693627bbd834238fd6fc1b0deb45a5fead4b0fa
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-01 01:17:37 +0000

    Deploying to master from @ dimaslanjaka/hexo-blogger-xml@a12edf12f37df3dee201b68011aac52cb37621c2 🚀

---
+++ gulpfile.ts
@@ -0,0 +1,17 @@
+import gulpCore from "./src/gulp-core";
+import { PostHeader } from "./src/types/post-header";
+function defaultTask(cb) {
+  gulpCore({
+    input: ["./xml/test.xml"],
+    output: "./build/export/gulp",
+    callback(content: string, headers: PostHeader): string {
+      console.log(headers.title);
+      return content;
+    },
+    hostname: ["webmanajemen.com", "web-manajemen.blogspot.com", "dimaslanjaka.github.io"],
+  });
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask;
