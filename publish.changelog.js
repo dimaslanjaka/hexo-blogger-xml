36291bb261464b2ccaf268927b2e5b062f4d7e5b
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-04 18:34:00 +0700

    Update changelog

--- publish.changelog.js
+++ publish.changelog.js
@@ -1,9 +1,6 @@
 const exec = require("child_process").exec;
 
-exec("git log --reflog", (err, stdout, stderr) => {
+exec('git log --reflog --pretty=format:"%h - %an, %ar : %s" --not --remotes', (err, stdout, stderr) => {
   console.log(stdout);
 });
-
-exec("git reflog", (err, stdout, stderr) => {
-  console.log(stdout);
-});
+//git log --pretty=format:"%h - %an, %ar : %s"
