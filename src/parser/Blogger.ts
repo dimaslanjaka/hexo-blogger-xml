af0c98b76c125ecf20e00385c2d21173fc381817
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-11 03:59:31 +0700

    Update index

--- src/parser/Blogger.ts
+++ src/parser/Blogger.ts
@@ -21,7 +21,7 @@ import getUsername from "./node-username";
 import { EventEmitter } from "events";
 import trim_whitespaces from "./trim_whitespaces";
 import remove_double_quotes from "./remove_double_quotes";
-import "js-prototypes/src/String";
+import "../../packages/js-prototypes/src/String";
 import langID from "./lang/id.json";
 
 interface objResult {
